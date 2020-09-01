24939a49b457270bcacdda301015b94f2634cc4c
tieuconghoa <tieuconghoa193@gmail.com>
2020-09-01 20:06:49 +0700

    fix giao diện

--- controllers/capacity_controller.php
+++ controllers/capacity_controller.php
@@ -1,33 +1,54 @@
-<?php 
+<?php
 
-    require_once('controllers/base_controller.php');
-    require_once('logic/capacity_logic.php');
-    require_once('entities/capacity.php');
+require_once('controllers/base_controller.php');
+require_once('logic/capacity_component_logic.php');
+require_once('logic/capacity_logic.php');
+require_once('entities/capacity_component.php');
+require_once('entities/capacity.php');
 
-    class CapacityController extends BaseController{
+class CapacityController extends BaseController
+{
 
-        public function __construct()
-        {
-            $this->folder = 'capacity';
-        }
-
-        public function add()
-        {
-            $capacityLogic = new CapacityLogic();
-
-            if (isset($_POST['submit'])) {
-                
-                $capacityValue = $_POST['capacity_value'];
-
-                $action = new Capacity(null, $capacityValue);
-    
-                $capacityLogic->addCapacity($action);
-                
-                header('Location: ./');
-            }
-            $this->render('add', array());
-        }
+    public function __construct()
+    {
+        $this->folder = 'capacity';
     }
 
+    public function add()
+    {
+        $capacityLogic = new CapacityLogic();
 
-?>
+        if (isset($_POST['submit'])) {
+
+            $capacityValue = $_POST['capacity_value'];
+
+            $action = new Capacity(null, $capacityValue);
+
+            $capacityLogic->addCapacity($action);
+
+            header('Location: ./');
+        }
+        $this->render('add', array());
+    }
+    public function addComponent()
+    {
+        $capacityComponentLogic = new CapacityComponentLogic();
+        $capacityLogic = new CapacityLogic();
+        $listCapacity = $capacityLogic->getAlCapacity();
+
+        $data = array(
+            'listCapacity' => $listCapacity
+        );
+
+        if (isset($_POST['submit'])) {
+
+            $capacityComponentValue = $_POST['capacity_component_value'];
+            $capacityId = $_POST['capacity_id'];
+            $capacityComponent = new CapacityComponent(null, $capacityId, $capacityComponentValue);
+
+            $capacityComponentLogic->addCapacityComponent($capacityComponent);
+            header('Location: ./');
+        }
+        $this->render('addComponent', $data);
+    }
+}
--- views/account/login.php
+++ views/account/login.php
@@ -4,7 +4,7 @@
             <h1>Welcome</h1>
             <span class="error"><?=isset($data['error']) ? $data['error'] :'' ?></span>
             <form class="form" method="POST">
-                <input type="text" name="username" placeholder="Username" value="<?=isset($_POST['username']) ? $_POST['username'] :'' ?>" required>
+                <input type="text" name="username" placeholder="Username" value="<?=isset($_POST['username']) ? $_POST['username'] :'' ?>" autofocus required>
                 <input type="password" name="password" placeholder="Password" required>
                 <span class="show-password"></span>
                 <button type="submit" name="submit" value="submit" id="login-button">Login</button>
